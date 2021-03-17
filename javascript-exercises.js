bfb7fc4936511dc6dfc2079cad4de5dd3426687f
AmalAldajah <51743412+AmalAldajah@users.noreply.github.com>
2021-03-17 16:11:25 +0200

    JavaScript Exercises

---
+++ javascript-exercises.js
@@ -0,0 +1,165 @@
+/*
+1
+Write a javaScript function that prints the sum of the numbers 1 to 10, using a while loop
+*/
+
+
+
+
+/*
+2
+Write a javaScript function that prints a multiplication table for number 10
+
+let number = 10;
+>>>>> Result:   1 * 10= 10
+                2 * 10= 20
+                .
+                .
+                10 * 10= 100
+*/
+
+
+
+
+
+
+/*
+3
+Write a javaScript function that returns the largest element in an array
+** DON'T use any built-in functions**
+
+let numbers = [5, 8, 66, 1 , 40, 13, 27];
+*/
+
+
+
+/*
+4
+Write a javaScript function that reverses an array
+** DON'T use any built-in functions**
+
+let months = [April, May, June, July, August];
+*/
+
+
+
+
+
+/*
+5
+Write a javaScript function that returns the elements on even index in an array
+
+let months = [April, May, June, July, August];
+>>>>> Result: May, July
+*/
+
+
+
+
+
+/*
+6
+Write a javaScript function that compute the sum of the numbers in an array, using a for-loop
+*/
+
+
+
+
+/*
+7
+Write a javaScript function that concatenates two arrays
+** DON'T use any built-in functions**
+*/
+
+
+
+
+
+/*
+8
+Write a javaScript function that sorts an array
+** DON'T use any built-in functions**
+*/
+
+
+
+
+
+
+/*
+9
+Write a javaScript function that sorts an array
+** DON'T use any built-in functions**
+*/
+
+
+
+
+
+
+
+/*
+10
+Write a javaScript function that sorts an array
+** DON'T use any built-in functions**
+
+let numbers = [8, 0, 1, 5, 3];
+>>>>> Result: [0, 1, 3, 5, 8];
+*/
+
+
+
+
+
+/*
+11
+Write a javaScript function that counts the number of vowel letters in the string
+*/
+
+
+
+
+/*
+12
+Write a javaScript function that checks numbers and put even and odd numbers in two different araays
+
+let numbers = [10, 5, 3, 8, 1, 6, 9];
+>>>>> Result:   Even numbers [10, 8, 6]
+                Odd numbers [5, 3, 1, 9]
+*/
+
+
+
+
+
+
+/*
+13
+Write a javaScript function that calculates the factorial of any number
+*/
+
+
+
+
+/*
+14
+Write a javaScript function that finds the second minmum number of an array
+** DON'T use any built-in functions**
+
+let numbers = [10, 5, 3, 8, 1, 6, 9];
+>>>>> Result:   3
+*/
+
+
+
+
+
+
+/*
+15
+Write a javaScript function that compares the length of words in an array and returns the longest word with length
+** DON'T use any built-in functions**
+
+let animals = ['Horse', 'Cat', 'Giraffe']
+>>>>> Result:   Giraffe 7
+*/
